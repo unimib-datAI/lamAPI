f640c2c3b1b1959926c0815c507096f2154f8dc5
roby-avo <r.avogadro@campus.unimib.it>
2024-09-28 16:51:05 +0200

    Refactor column classification logic in ColumnAnalysis class

--- api/model/data_retrievers/column_analysis.py
+++ api/model/data_retrievers/column_analysis.py
@@ -25,24 +25,23 @@ class ColumnAnalysis:
             # Determine the classification and tag
             classification = col_info['classification']
             probabilities = col_info["probabilities"]
+            
+            NE_types = ["PERSON", "ORGANIZATION", "LOCATION", "OTHER"]
+            lit_types = ["NUMBER", "DATE", "STRING"]
 
-            if classification == "NUMBER":
+            if classification in lit_types:
                 tag = "LIT"
-                classification_type = "NUMBER"
-                datatype = "NUMBER"  # For LIT, always set datatype as NUMBER
-            elif classification == "DATE":
-                tag = "LIT"
-                classification_type = "DATE"
-                datatype = "DATE"
-            elif classification == "LOCATION":
+                classification_type = classification
+                datatype = classification
+            elif classification in NE_types:
                 tag = "NE"
-                classification_type = "ENTITY"
-                # For NE, pick the datatype based on the winning probability
-                datatype = max(probabilities, key=probabilities.get)
+                classification_type = classification
+                datatype = classification
             else:
-                tag = "LIT"
-                classification_type = "STRING"  # Fallback for other cases
-                datatype = "STRING"  # Default STRING for non-NE types
+                tag = "UNKNOWN"
+                classification_type = "UNKNOWN"
+                datatype = "UNKNOWN"    
+           
             
             # Add the transformed data to the adapted output, including probabilities
             adapted_output[col_idx] = {
