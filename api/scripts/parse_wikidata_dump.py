c073f290ae38ba07d368f18f74d5c17f1b46e748
belerico <belo.fede@outlook.com>
2024-10-30 09:36:49 +0000

    Fix indexing

--- api/scripts/parse_wikidata_dump.py
+++ api/scripts/parse_wikidata_dump.py
@@ -1,36 +1,41 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 import bz2
 import json
 import os
 import sys
 import traceback
+from datetime import datetime
+
 from pymongo import MongoClient
 from tqdm import tqdm
-from datetime import datetime
 
 
 def create_indexes(db):
     # Specify the collections and their respective fields to be indexed
     index_specs = {
-        'cache': ['cell', 'lastAccessed'],  # Example: Indexing 'cell' and 'type' fields in 'cache' collection
-        'items': ['id_entity', 'entity', 'category', 'popularity'],
-        'literals': ['id_entity', 'entity'],
-        'mappings': ['curid', 'wikipedia_id', 'wikidata_id', 'dbpedia_id'],
-        'objects': ['id_entity', 'entity'],
-        'types': ['id_entity', 'entity']
+        "cache": ["cell", "lastAccessed"],  # Example: Indexing 'cell' and 'type' fields in 'cache' collection
+        "items": ["id_entity", "entity", "category", "popularity"],
+        "literals": ["id_entity", "entity"],
+        "mappings": ["curid", "wikipedia_id", "wikidata_id", "dbpedia_id"],
+        "objects": ["id_entity", "entity"],
+        "types": ["id_entity", "entity"],
     }
 
     for collection, fields in index_specs.items():
         if collection == "cache":
-            db[collection].create_index([('cell', 1), ('fuzzy', 1), ('type', 1), ('kg', 1), ('limit', 1)], unique=True)
+            db[collection].create_index([("cell", 1), ("fuzzy", 1), ("type", 1), ("kg", 1), ("limit", 1)], unique=True)
         elif collection == "items":
-            db[collection].create_index([('entity', 1), ('category', 1)], unique=True)    
+            db[collection].create_index([("entity", 1), ("category", 1)], unique=True)
         for field in fields:
             db[collection].create_index([(field, 1)])  # 1 for ascending order
 
 
 # Initial Estimation
 initial_estimated_average_size = 800  # Initial average size in bytes, can be adjusted
-BATCH_SIZE = 100 # Number of entities to insert in a single batch
+BATCH_SIZE = 100  # Number of entities to insert in a single batch
 
 if len(sys.argv) < 2:
     print("Usage: python script_name.py <path_to_wikidata_dump>")
@@ -44,6 +49,7 @@ file = bz2.BZ2File(file_path, "r")
 
 # MongoDB connection setup
 MONGO_ENDPOINT, MONGO_ENDPOINT_PORT = os.environ["MONGO_ENDPOINT"].split(":")
+MONGO_ENDPOINT = "localhost"
 MONGO_ENDPOINT_PORT = int(MONGO_ENDPOINT_PORT)
 MONGO_ENDPOINT_USERNAME = os.environ["MONGO_INITDB_ROOT_USERNAME"]
 MONGO_ENDPOINT_PASSWORD = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
@@ -51,47 +57,38 @@ current_date = datetime.now()
 formatted_date = current_date.strftime("%d%m%Y")
 DB_NAME = f"wikidata{formatted_date}"
 
-client = MongoClient(MONGO_ENDPOINT, MONGO_ENDPOINT_PORT, username=MONGO_ENDPOINT_USERNAME, password=MONGO_ENDPOINT_PASSWORD)
+client = MongoClient(
+    MONGO_ENDPOINT, MONGO_ENDPOINT_PORT, username=MONGO_ENDPOINT_USERNAME, password=MONGO_ENDPOINT_PASSWORD
+)
 log_c = client.wikidata.log
 items_c = client[DB_NAME].items
 objects_c = client[DB_NAME].objects
 literals_c = client[DB_NAME].literals
 types_c = client[DB_NAME].types
 
-c_ref = {
-    "items": items_c,
-    "objects":objects_c, 
-    "literals":literals_c, 
-    "types":types_c
-}
+c_ref = {"items": items_c, "objects": objects_c, "literals": literals_c, "types": types_c}
 
 create_indexes(client[DB_NAME])
 
-buffer = {
-    "items": [],
-    "objects": [], 
-    "literals": [], 
-    "types": []
-}
+buffer = {"items": [], "objects": [], "literals": [], "types": []}
 
 DATATYPES_MAPPINGS = {
-    'external-id':'STRING',
-    'quantity': 'NUMBER',
-    'globe-coordinate': 'STRING',
-    'string': 'STRING',
-    'monolingualtext': 'STRING',
-    'commonsMedia': 'STRING',
-    'time': 'DATETIME',
-    'url': 'STRING',
-    'geo-shape': 'GEOSHAPE',
-    'math': 'MATH',
-    'musical-notation': 'MUSICAL_NOTATION',
-    'tabular-data': 'TABULAR_DATA'
+    "external-id": "STRING",
+    "quantity": "NUMBER",
+    "globe-coordinate": "STRING",
+    "string": "STRING",
+    "monolingualtext": "STRING",
+    "commonsMedia": "STRING",
+    "time": "DATETIME",
+    "url": "STRING",
+    "geo-shape": "GEOSHAPE",
+    "math": "MATH",
+    "musical-notation": "MUSICAL_NOTATION",
+    "tabular-data": "TABULAR_DATA",
 }
 DATATYPES = list(set(DATATYPES_MAPPINGS.values()))
 total_size_processed = 0
 num_entities_processed = 0
-
 
 
 def update_average_size(new_size):
@@ -106,12 +103,8 @@ def check_skip(obj, datatype):
     if "datavalue" not in temp:
         return True
 
-    skip = {
-        "wikibase-lexeme",
-        "wikibase-form",
-        "wikibase-sense"
-    }
-    
+    skip = {"wikibase-lexeme", "wikibase-form", "wikibase-sense"}
+
     return datatype in skip
 
 
@@ -140,17 +133,17 @@ def flush_buffer(buffer):
         if len(buffer[key]) > 0:
             c_ref[key].insert_many(buffer[key])
             buffer[key] = []
-            
-            
+
+
 def parse_data(item, i):
     entity = item["id"]
     labels = item.get("labels", {})
     aliases = item.get("aliases", {})
-    description = item.get('descriptions', {}).get('en', {})
+    description = item.get("descriptions", {}).get("en", {})
     category = "entity"
     sitelinks = item.get("sitelinks", {})
     popularity = len(sitelinks) if len(sitelinks) > 0 else 1
-    
+
     all_labels = {}
     for lang in labels:
         all_labels[lang] = labels[lang]["value"]
@@ -184,23 +177,11 @@ def parse_data(item, i):
             "aliases": all_aliases,
             "types": types,
             "popularity": popularity,
-            "category": category
+            "category": category,
         },
-        "objects": { 
-            "id_entity": i,
-            "entity": entity,
-            "objects":objects
-        },
-        "literals": { 
-            "id_entity": i,
-            "entity": entity,
-            "literals": literals
-        },
-        "types": { 
-            "id_entity": i,
-            "entity": entity,
-            "types": types
-        },
+        "objects": {"id_entity": i, "entity": entity, "objects": objects},
+        "literals": {"id_entity": i, "entity": entity, "literals": literals},
+        "types": {"id_entity": i, "entity": entity, "types": types},
     }
 
     predicates = item["claims"]
@@ -219,23 +200,23 @@ def parse_data(item, i):
 
                 if value not in objects:
                     objects[value] = []
-                objects[value].append(predicate)    
+                objects[value].append(predicate)
             else:
-                value = get_value(obj, datatype)                
+                value = get_value(obj, datatype)
                 lit = literals[DATATYPES_MAPPINGS[datatype]]
 
                 if predicate not in lit:
                     lit[predicate] = []
-                lit[predicate].append(value)   
+                lit[predicate].append(value)
 
     for key in buffer:
-        buffer[key].append(join[key])            
+        buffer[key].append(join[key])
 
     if len(buffer["items"]) == BATCH_SIZE:
         flush_buffer(buffer)
 
 
-def parse_wikidata_dump():            
+def parse_wikidata_dump():
     global initial_total_lines_estimate
     pbar = tqdm(total=initial_total_lines_estimate)
     for i, line in enumerate(file):
