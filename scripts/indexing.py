3249c6242f9f7adfc96c40f9e4d941296063823c
belerico <belo.fede@outlook.com>
2025-01-20 18:55:48 +0000

    Format

--- scripts/indexing.py
+++ scripts/indexing.py
@@ -1,21 +1,23 @@
-import os
-import time
 import json
-import sys
-import traceback
+import os
 import re
+import sys
+import time
+import traceback
+from multiprocessing import Pool, cpu_count
+
 from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk, BulkIndexError
+from elasticsearch.helpers import BulkIndexError, bulk
 from pymongo import MongoClient
 from tqdm import tqdm
-from multiprocessing import Pool, cpu_count
+
 
 def index_documents(es_host, es_port, buffer, max_retries=5):
     es = Elasticsearch(
-        hosts=f'http://{es_host}:{es_port}',
+        hosts=f"http://{es_host}:{es_port}",
         request_timeout=60,
-        max_retries=10, 
-        retry_on_timeout=True
+        max_retries=10,
+        retry_on_timeout=True,
     )
     for attempt in range(max_retries):
         try:
@@ -29,11 +31,14 @@ def index_documents(es_host, es_port, buffer, max_retries=5):
                 print(f"Error details: {error_info}")
             time.sleep(5)
         except Exception as e:
-            print(f"An unexpected error occurred during indexing on attempt {attempt + 1}: {str(e)}")
+            print(
+                f"An unexpected error occurred during indexing on attempt {attempt + 1}: {str(e)}"
+            )
             traceback.print_exc()
             time.sleep(5)
     else:
         print("Max retries exceeded. Failed to index some documents.")
+
 
 def generate_dot_notation_options(name):
     words = name.split()
@@ -44,29 +49,34 @@ def generate_dot_notation_options(name):
         abbreviated_parts = []
         for j in range(num_words - 1):
             if j < i:
-                abbreviated_parts.append(words[j][0] + '.')
+                abbreviated_parts.append(words[j][0] + ".")
             else:
                 abbreviated_parts.append(words[j])
-        
-        option = ' '.join(abbreviated_parts + [words[-1]])
+
+        option = " ".join(abbreviated_parts + [words[-1]])
         options.append(option)
-    
+
     return options
+
 
 def create_elasticsearch_client(endpoint, port):
     return Elasticsearch(
-        hosts=f'http://{endpoint}:{port}',
+        hosts=f"http://{endpoint}:{port}",
         request_timeout=60,
-        max_retries=10, 
-        retry_on_timeout=True
+        max_retries=10,
+        retry_on_timeout=True,
     )
+
 
 def create_mongo_client(endpoint, port):
     return MongoClient(endpoint, int(port))
 
+
 def print_usage():
     print("Usage:")
-    print("  python indexing_script.py index <DB_NAME> <COLLECTION_NAME> <MAPPING_FILE>")
+    print(
+        "  python indexing_script.py index <DB_NAME> <COLLECTION_NAME> <MAPPING_FILE>"
+    )
     print("  python indexing_script.py status")
     print("  python indexing_script.py list_databases")
     print("  python indexing_script.py list_collections <DB_NAME>")
@@ -75,11 +85,22 @@ def print_usage():
     print("  <COLLECTION_NAME>  : The name of the MongoDB collection to index.")
     print("  <MAPPING_FILE>     : The path to the Elasticsearch mapping JSON file.")
 
+
 def process_batch(args):
     es_host, es_port, batch = args
     index_documents(es_host, es_port, batch)
 
-def index_data(es_host, es_port, mongo_client, db_name, collection_name, mapping, batch_size=100000, max_threads=None):
+
+def index_data(
+    es_host,
+    es_port,
+    mongo_client,
+    db_name,
+    collection_name,
+    mapping,
+    batch_size=100000,
+    max_threads=None,
+):
     if max_threads is None:
         max_threads = cpu_count() - 1
 
@@ -90,23 +111,30 @@ def index_data(es_host, es_port, mongo_client, db_name, collection_name, mapping
         max_popularity = max_popularity_doc["popularity"]
         print(f"The maximum popularity is: {max_popularity}")
     else:
-        raise Exception("No documents found in the collection or popularity field is missing.")
+        raise Exception(
+            "No documents found in the collection or popularity field is missing."
+        )
 
-    index_name = re.sub(r'\d+$', '', db_name)
+    index_name = re.sub(r"\d+$", "", db_name)
     es_client = create_elasticsearch_client(es_host, es_port)
     if es_client.indices.exists(index=index_name):
         print(f"Index {index_name} exists. Deleting it...")
         es_client.indices.delete(index=index_name)
-    
+
     print(f"Creating index {index_name}...")
-    es_client.indices.create(index=index_name, settings=mapping["settings"], mappings=mapping["mappings"])
+    es_client.indices.create(
+        index=index_name, settings=mapping["settings"], mappings=mapping["mappings"]
+    )
 
     # Disable refresh interval and replicas temporarily
-    es_client.indices.put_settings(index=index_name, settings={"index": {"refresh_interval": "-1", "number_of_replicas": 0}})
-    
+    es_client.indices.put_settings(
+        index=index_name,
+        settings={"index": {"refresh_interval": "-1", "number_of_replicas": 0}},
+    )
+
     total_docs = documents_c.estimated_document_count()
     results = documents_c.find({})
-    
+
     buffer = []
     batches = []
     pbar = tqdm(total=total_docs, desc="Indexing documents")
@@ -131,10 +159,16 @@ def index_data(es_host, es_port, mongo_client, db_name, collection_name, mapping
         for lang, alias_list in aliases.items():
             for alias in alias_list:
                 key = alias.lower()
-                if key in unique_labels and not unique_labels[key]["is_alias"]:  # Skip if the alias is already a label
+                if (
+                    key in unique_labels and not unique_labels[key]["is_alias"]
+                ):  # Skip if the alias is already a label
                     continue
                 if key not in unique_labels:
-                    unique_labels[key] = {"name": alias, "languages": [], "is_alias": True}
+                    unique_labels[key] = {
+                        "name": alias,
+                        "languages": [],
+                        "is_alias": True,
+                    }
                 unique_labels[key]["languages"].append(lang)
 
         all_names = []
@@ -142,14 +176,18 @@ def index_data(es_host, es_port, mongo_client, db_name, collection_name, mapping
             name = value["name"]
             languages = value["languages"]
             is_alias = value["is_alias"]
-            all_names.append({"name": name, "language": languages, "is_alias": is_alias})
+            all_names.append(
+                {"name": name, "language": languages, "is_alias": is_alias}
+            )
 
         if NERtype == "PERS":
             name = labels.get("en")
             if name is not None:
                 name_abbreviations = generate_dot_notation_options(name)
                 for abbrev in name_abbreviations:
-                    all_names.append({"name": abbrev, "language": ["en"], "is_alias": True})
+                    all_names.append(
+                        {"name": abbrev, "language": ["en"], "is_alias": True}
+                    )
 
         for name_entry in all_names:
             name = name_entry["name"]
@@ -168,8 +206,8 @@ def index_data(es_host, es_port, mongo_client, db_name, collection_name, mapping
                 "NERtype": NERtype,
                 "types": " ".join(types),
                 "length": len(name),
-                "ntoken": len(name.split(' ')),
-                "popularity": round(popularity / max_popularity, 2)
+                "ntoken": len(name.split(" ")),
+                "popularity": round(popularity / max_popularity, 2),
             }
             _id += 1
             buffer.append(doc)
@@ -180,11 +218,13 @@ def index_data(es_host, es_port, mongo_client, db_name, collection_name, mapping
 
                 if len(batches) >= max_threads:
                     with Pool(max_threads) as pool:
-                        pool.map(process_batch, [(es_host, es_port, batch) for batch in batches])
+                        pool.map(
+                            process_batch,
+                            [(es_host, es_port, batch) for batch in batches],
+                        )
                     batches = []
 
         pbar.update(1)
-       
 
     if len(buffer) > 0:
         batches.append(buffer)
@@ -192,10 +232,13 @@ def index_data(es_host, es_port, mongo_client, db_name, collection_name, mapping
     if len(batches) > 0:
         with Pool(max_threads) as pool:
             pool.map(process_batch, [(es_host, es_port, batch) for batch in batches])
-    
+
     pbar.close()
     # Enable refresh interval
-    es_client.indices.put_settings(index=index_name, settings={"index": {"refresh_interval": "1s"}})
+    es_client.indices.put_settings(
+        index=index_name, settings={"index": {"refresh_interval": "1s"}}
+    )
+
 
 def show_status(mongo_client, es):
     print("MongoDB Status:")
@@ -203,10 +246,12 @@ def show_status(mongo_client, es):
     print("\nElasticsearch Status:")
     print(es.info())
 
+
 def list_databases(mongo_client):
     print("Available Databases:")
     for db in mongo_client.list_database_names():
         print(f"  - {db}")
+
 
 def list_collections(mongo_client, db_name):
     if db_name in mongo_client.list_database_names():
@@ -215,6 +260,7 @@ def list_collections(mongo_client, db_name):
             print(f"  - {coll}")
     else:
         print(f"Database '{db_name}' not found.")
+
 
 def main():
     if len(sys.argv) < 2:
@@ -233,22 +279,29 @@ def main():
             if len(sys.argv) != 5:
                 print_usage()
                 sys.exit(1)
-            
+
             db_name = sys.argv[2]
             collection_name = sys.argv[3]
             mapping_file = sys.argv[4]
 
-            with open(mapping_file, 'r') as file:
+            with open(mapping_file, "r") as file:
                 mapping = json.load(file)
 
             # Perform indexing
-            index_data(ELASTIC_ENDPOINT, ELASTIC_PORT, mongo_client, db_name, collection_name, mapping)
+            index_data(
+                ELASTIC_ENDPOINT,
+                ELASTIC_PORT,
+                mongo_client,
+                db_name,
+                collection_name,
+                mapping,
+            )
 
-            print('All Finished')
+            print("All Finished")
 
         elif action == "status":
             show_status(mongo_client, es)
-        
+
         elif action == "list_databases":
             list_databases(mongo_client)
 
@@ -268,6 +321,7 @@ def main():
         traceback.print_exc()
         print("An error occurred. Exiting...")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
